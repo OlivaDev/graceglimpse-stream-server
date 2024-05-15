aa5d09cb22950260528e79b568b98a5e606eb077
OlivaDev <113021612+OlivaDev@users.noreply.github.com>
2024-05-14 22:40:40 -0400

    Sockets funcionando correctamente

--- server.js
+++ server.js
@@ -19,55 +19,28 @@ app.use(bodyParser.json({limit: "50mb"}))
 
 const io = new serv.Server(server, {cors:{origin: "*"}})
 
-app.get('/video', (req, res) => {
-    const range = req.headers.range
-    if (!range)
-        res.status(400).send('error')
-
-    const videoPath = 'public/frag_bunny.mp4'
-    const videoSize = fs.statSync(videoPath).size
-
-    const chunkSize = 10 ** 6
-    // bytes=64165
-    const start = Number(range.replace(/\D/g, ''))
-    const end = Math.min(start + chunkSize, videoSize - 1)
-    const contentLength = end - start + 1
-    const headers = {
-        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-        "Accept-Ranges": 'bytes',
-        "Content-Length": contentLength,
-        "Content-Type": 'video/mp4'
-    }
-
-    res.writeHead(206, headers)
-
-    const videoStream = fs.createReadStream(videoPath, { start, end })
-
-    videoStream.pipe(res)
-})
+const sockets = {}
 
 io.on('connection', (socket) => {
     console.log('Conectado al socket');
-    /*
-    socket.on("RTCanswer", (req) => {
-        io.sockets.emit("RTCanswer", req)
-    })
 
-    socket.on("newRTC", (req) => {
-        io.sockets.emit("newRTC", req)
+    socket.on("saveSocket",(id) => {
+        sockets[id] = socket.id
     })
-    */
 
     socket.on("joinStream", (req) => {
-        io.sockets.emit("joinStream", req)
+        let streamSocket = sockets[req.streamer]
+        io.sockets.sockets.get(streamSocket).emit("joinStream", req)
     })
 
     socket.on("joinAccepted", (req) => {
-        io.sockets.emit("joinAccepted", req)
+        let userSocket = sockets[req.user]
+        io.sockets.sockets.get(userSocket).emit("joinAccepted", req)
     })
 
     socket.on("joinAnswer", (req) => {
-        io.sockets.emit("joinAnswer", req)
+        let streamer = sockets[req.streamer]
+        io.sockets.sockets.get(streamer).emit("joinAnswer", req)
     })
 })
 
